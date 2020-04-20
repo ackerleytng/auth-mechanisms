84d48eb91ee07ae6b59cfebd7e7d497adfd2f141
ackerleytng <ackerleytng@gmail.com>
2020-04-20 14:37:02 +0800

    Restore routing using HostSNI

--- exploration/compose/start.sh
+++ exploration/compose/start.sh
@@ -18,7 +18,7 @@ update-ca-certificates
 # Wait for keycloak to be ready (use a direct connection to the container to
 #   test, because traefik (keycloak.localhost) comes up way before the service
 #   is ready
-wait-for-it.sh keycloak:8443
+wait-for-it.sh -t 60 keycloak:8443
 
 # Sets up this client with keycloak
 client_secret=$(/gatekeeper/setup.sh)
