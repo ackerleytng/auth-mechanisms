5d4ac6545e80d5298a4028484139ce2afd2289b1
ackerleytng <ackerleytng@gmail.com>
2020-04-20 18:05:40 +0800

    Permit unauthenticated access to /

--- exploration/ace/src/main/java/com/example/ace/AceApplication.java
+++ exploration/ace/src/main/java/com/example/ace/AceApplication.java
@@ -1,7 +1,12 @@
 package com.example.ace;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AceApplication {
@@ -10,4 +15,16 @@ public class AceApplication {
         SpringApplication.run(AceApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            final String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (final String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
 }
--- exploration/ace/src/main/java/com/example/ace/AceWebSecurityConfigurerAdapter.java
+++ exploration/ace/src/main/java/com/example/ace/AceWebSecurityConfigurerAdapter.java
@@ -1,17 +1,17 @@
 package com.example.ace;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class AceWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/**")
-            .permitAll();
+            .antMatchers("/").permitAll()
+            .anyRequest().authenticated();
     }
 
 }
