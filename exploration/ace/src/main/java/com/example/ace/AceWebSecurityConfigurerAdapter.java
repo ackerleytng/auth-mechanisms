6e65bf6f980c337a736c7c2b7d8a8e5c624d74c2
ackerleytng <ackerleytng@gmail.com>
2020-04-20 18:23:33 +0800

    Modify gatekeeper config to do authorization, configure ace for access token

--- exploration/ace/src/main/java/com/example/ace/AceWebSecurityConfigurerAdapter.java
+++ exploration/ace/src/main/java/com/example/ace/AceWebSecurityConfigurerAdapter.java
@@ -3,15 +3,17 @@ package com.example.ace;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
 public class AceWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/").permitAll()
-            .anyRequest().authenticated();
+        http.authorizeRequests(authorize -> authorize
+                               .antMatchers("/").permitAll()
+                               .anyRequest().authenticated())
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 
 }
